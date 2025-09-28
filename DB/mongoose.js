e85dc0a3b9c213c32e4b0b0c16f342c629a4d090
ahmed <ahmedaty957@gmail.com>
2025-09-28 21:23:22 +0300

    lec1.1

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({path:"../.env"});
 
-mongoose.connect(process.env.MONGOURL, ).then(() => {
+console.log("Mongo URL:", process.env.MONGOURL); // Debugging line to check if MONGOURL is loaded correctly
+
+mongoose.connect(process.env.MONGOURL).then(() => {
     console.log("DB Connection Successful");
 }).catch((err) => {
     console.log(err.message);
