24e189e0a228ea4d6c9a5d2469191893432055e8
ahmed <ahmedaty957@gmail.com>
2025-10-01 10:18:47 +0300

    edit .gitignore

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({path:"../.env"});
 
-console.log("Mongo URL:", process.env.MONGOURL); // Debugging line to check if MONGOURL is loaded correctly
-
 mongoose.connect(process.env.MONGOURL).then(() => {
     console.log("DB Connection Successful");
 }).catch((err) => {
